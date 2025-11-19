4f8fec867d5fa7bcaeda519e1573356844df41a9
l-yynnee <ndanlinet@gmail.com>
2025-11-19 13:26:46 +0300

    added authentication logic to index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+if (req.session.authorization) {
+        let token = req.session.authorization['accessToken'];
+        // Verify JWT token
+        jwt.verify(token, "access", (err, user) => {
+            if (!err) {
+                req.user = user;
+                next(); // Proceed to the next middleware
+            } else {
+                return res.status(403).json({ message: "User not authenticated" });
+            }
+        });
+    } else {
+        return res.status(403).json({ message: "User not logged in" });
+    }
+});
